3dfaf960ee4d6222c011cffaa96ad49e25e85469
Sergey Lukin <contact@sergeylukin.com>
2015-10-03 16:52:23 +0300

    Fix Disqus variables access

--- app/_assets/javascripts/components/comments-button.js
+++ app/_assets/javascripts/components/comments-button.js
@@ -1,9 +1,12 @@
 var buttons = document.querySelector('.js-show-comments-button');
 if (buttons) {
   buttons.addEventListener('click', function() {
-    var that = this,
-        disqus_shortname = that.dataset.disqusShortname,
-        disqus_identifier = that.dataset.disqusIdentifier;
+    var that = this;
+
+    // We need these variables be global in order for disqus embed
+    // code to be able to read them
+    disqus_shortname = that.dataset.disqusShortname;
+    disqus_identifier = that.dataset.disqusIdentifier;
 
     var s = document.createElement("script"),
         url = "http://" + disqus_shortname + ".disqus.com/embed.js";
